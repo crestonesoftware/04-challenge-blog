498cbf99d157099e9a64dc66ede79865ec3e1a9c
cuhrmunken <elliott.davis@crestonetech.com>
2024-06-06 19:16:08 -0700

    styles blog header, toggles dark/light for blog

--- assets/javascript/blog.js
+++ assets/javascript/blog.js
@@ -1,1 +1,33 @@
-alert("Listo para desarrollar, compadre");
+// ENUMS
+const DISPLAY_MODE_LIGHT = "light";
+const DISPLAY_MODE_DARK = "dark";
+
+// DEPENDENCIES (ELEMENTS)
+const bodyEl        = $('body');
+const sunButton     = $('#display-mode-toggle');
+
+// DATA
+let displayMode = DISPLAY_MODE_LIGHT;
+
+// FUNCTIONS
+
+
+function handleSunButtonClick(event) {
+    console.log('asdsad');
+    // get all elements marked with the class .toggle-display. They include
+    // - the body 
+    // - any element that doesn't inherit its color and background color 
+    //   from the body, e.g. the button
+    const toggleElements = $('.toggle-display');
+
+    if (DISPLAY_MODE_DARK == displayMode) {
+        displayMode = DISPLAY_MODE_LIGHT; // toggle the attribute on <body>
+        // swap light and dark classes
+        toggleElements.addClass('mode-light').removeClass('mode-dark');
+    } else {
+        displayMode = DISPLAY_MODE_DARK; // toggle the attribute on <body>
+        // swap light and dark classes
+        toggleElements.addClass('mode-dark').removeClass('mode-light');
+    }
+}
+sunButton.on('click', handleSunButtonClick);
