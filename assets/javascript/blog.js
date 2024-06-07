482ca41bc11c8070670a7ef0589f7f213df3afa6
cuhrmunken <elliott.davis@crestonetech.com>
2024-06-07 15:07:53 -0600

    retrieves entries and displays in div

--- assets/javascript/blog.js
+++ assets/javascript/blog.js
@@ -1,6 +1,47 @@
+// DEPEPENDENCIES
+const blogEntryContainer = $('#blog-entry-container');
+
 // FUNCTIONS
 function loadBlogEntries() {
     console.log("this happened on load");
+    const blogEntries = COMMON.fetchBLogEntries();
+    for (const entry of blogEntries) {
+        
+        //const blogEntryElement = composeBlogEntryElement(entry);
+        const blogEntryElement = composeBlogEntryElement(entry);
+        blogEntryContainer.append(blogEntryElement);
+    }
+}
+
+function composeBlogEntryElement(entry) {
+    // const newEntryElement = $(`<h1>${entry.title}</h1>`);
+    const newEntryElement = $(`
+    <div class="blog-entry">
+        <h4>${entry.title}</h4>
+        <div></div>
+        <p class="blog-content mb-half-em">${entry.content}</p>
+        <p class="blog-username mb-half-em">Posted by: <span>${entry.username}</span></p>
+    </div>
+    `);
+    // <div class="blog-entry">
+    //     <h4>Blog Title</h4>
+        // <div></div>
+        // <p class="blog-content mb-half-em">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia nesciunt sapiente neque dolores! Adipisci illo maiores placeat explicabo? Optio aliquid quam at maiores commodi vero. Enim, ducimus. Laborum, tempore quo!</p>
+        // <p class="blog-username mb-half-em">Posted by: <span>username</span></p>
+    // </div>
+
+
+    return newEntryElement;
+    // 
+    // <div></div>
+    // <p class="blog-content mb-half-em">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia nesciunt sapiente neque dolores! Adipisci illo maiores placeat explicabo? Optio aliquid quam at maiores commodi vero. Enim, ducimus. Laborum, tempore quo!</p>
+    // <p class="blog-username mb-half-em">Posted by: <span>username</span></p>
+    // ');
+
+}
+
+function blogEntryElement(element) {
+
 }
 
 // USER INTERACTIONS
