4a985105b49f39c80d2a8c865edb617c3f590e27
cuhrmunken <elliott.davis@crestonetech.com>
2024-06-08 18:17:57 -0600

    clean up and consolidate common elements to logic.js

--- assets/javascript/form.js
+++ assets/javascript/form.js
@@ -4,47 +4,36 @@ const usernameInput = $('#username-input');
 const titleInput    = $('#title-input');
 const contentInput  = $('#content-input');          
 
-// DATA
-
-// TEST ONLY - REMOVE BEFORE RELEASE. 
-function testData_blogForm() {
-    usernameInput.val("Alistair McKenzie");
-    titleInput.val('A solid blogging');
-    contentInput.val('Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium excepturi fugit illum explicabo id rem, quam ipsa quas nesciunt quidem nobis laboriosam iusto ad eius! Facere animi ex adipisci veniam!');
-    
-}
-// TEST - REMOVE BEFORE RELEASE
-testData_blogForm();
-
 // FUNCTIONS
-function handleSunButtonClick(event) {
-    // get all elements marked with the class .toggle-display. They include
-    // - the body 
-    // - any element that doesn't inherit its color and background color 
-    //   from the body, e.g. the button
-    const toggleElements = $('.toggle-display');
+// function handleSunButtonClick(event) {
+//     // get all elements marked with the class .toggle-display. They include
+//     // - the body 
+//     // - any element that doesn't inherit its color and background color 
+//     //   from the body, e.g. the button
+//     const toggleElements = $('.toggle-display');
 
-    if (COMMON.DISPLAY_MODE_DARK == displayMode) {
-        displayMode = COMMON.DISPLAY_MODE_LIGHT; // toggle the attribute on <body>
-        // swap light and dark classes
-        toggleElements.addClass('mode-light').removeClass('mode-dark');
-    } else {
-        displayMode = COMMON.DISPLAY_MODE_DARK; // toggle the attribute on <body>
-        // swap light and dark classes
-        toggleElements.addClass('mode-dark').removeClass('mode-light');
-    }
-}
+//     if (COMMON.DISPLAY_MODE_DARK == displayMode) {
+//         displayMode = COMMON.DISPLAY_MODE_LIGHT; // toggle the attribute on <body>
+//         // swap light and dark classes
+//         toggleElements.addClass('mode-light').removeClass('mode-dark');
+//     } else {
+//         displayMode = COMMON.DISPLAY_MODE_DARK; // toggle the attribute on <body>
+//         // swap light and dark classes
+//         toggleElements.addClass('mode-dark').removeClass('mode-light');
+//     }
+// }
 
 function handleFormSubmit(event) {
     event.preventDefault();
-    
+    // require data in all fields
     if(!usernameInput.val() || !titleInput.val() || !contentInput.val()) {
         alert("Username, Title, and Content are all required");
         return;
     }
+    // store the Blog Entry, then redirect to the Blog page to see it
     COMMON.addBlogEntry(usernameInput.val(), titleInput.val(), contentInput.val());
     location.href = 'blog.html';
 }
 
-sunButton.on('click', handleSunButtonClick);
+sunButton.on('click', COMMON.handleSunButtonClick);
 submitButton.on('click', handleFormSubmit);
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,14 +1,13 @@
 // ENUMS
-const DISPLAY_MODE_LIGHT = "light";
-const DISPLAY_MODE_DARK = "dark";
+// const DISPLAY_MODE_LIGHT = "light";
+// const DISPLAY_MODE_DARK = "dark";
 
 // DEPENDENCIES (ELEMENTS)
 const bodyEl        = $('body');
 const sunButton     = $('#display-mode-toggle');
 
 // DATA
-let displayMode = DISPLAY_MODE_LIGHT;
-const ENTRIES_IN_LOCAL_STORAGE = "blogEntries";
+// let displayMode = COMMON.DISPLAY_MODE_LIGHT;
 
 const COMMON = {
     DISPLAY_MODE_LIGHT: "light",
@@ -16,7 +15,7 @@ const COMMON = {
     ENTRIES_IN_LOCAL_STORAGE: "blogEntries",
 
     fetchBLogEntries: function() {
-        const blogEntries = JSON.parse(localStorage.getItem(ENTRIES_IN_LOCAL_STORAGE));
+        const blogEntries = JSON.parse(localStorage.getItem(this.ENTRIES_IN_LOCAL_STORAGE));
         if (!blogEntries)
             return [];
         console.log(blogEntries);
@@ -33,13 +32,24 @@ const COMMON = {
     
         blogEntries.push(blogEntry);
         localStorage.setItem(COMMON.ENTRIES_IN_LOCAL_STORAGE,JSON.stringify(blogEntries));
-    }
+    },   
+    handleSunButtonClick: function(event) {
+        // get all elements marked with the class .toggle-display. They include
+        // - the body 
+        // - any element that doesn't inherit its color and background color 
+        //   from the body, e.g. the button
+        const toggleElements = $('.toggle-display');
     
-}
-function wwfetchBLogEntries() {
-    const blogEntries = JSON.parse(localStorage.getItem(ENTRIES_IN_LOCAL_STORAGE));
-    console.log(blogEntries);
-    return blogEntries;
+        if (COMMON.DISPLAY_MODE_DARK == displayMode) {
+            displayMode = COMMON.DISPLAY_MODE_LIGHT; // toggle the attribute on <body>
+            // swap light and dark classes
+            toggleElements.addClass('mode-light').removeClass('mode-dark');
+        } else {
+            displayMode = COMMON.DISPLAY_MODE_DARK; // toggle the attribute on <body>
+            // swap light and dark classes
+            toggleElements.addClass('mode-dark').removeClass('mode-light');
+        }
+    }
 }
 
-
+let displayMode = COMMON.DISPLAY_MODE_LIGHT;
